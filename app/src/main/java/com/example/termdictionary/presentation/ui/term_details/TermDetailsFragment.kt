d97fdcfe8215c82fa78cccbd3753512655ab7955
Firdavs <firdavs_20111@mail.ru>
2022-04-07 02:04:35 +0300

    added navigation drawer, search and filter actions

--- app/src/main/java/com/example/termdictionary/presentation/ui/term_details/TermDetailsFragment.kt
+++ app/src/main/java/com/example/termdictionary/presentation/ui/term_details/TermDetailsFragment.kt
@@ -3,14 +3,16 @@ package com.example.termdictionary.presentation.ui.term_details
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.termdictionary.R
 import com.example.termdictionary.databinding.FragmentTermDetailsBinding
 import com.example.termdictionary.presentation.model.TermUI
 
-class TermDetailsFragment: Fragment(R.layout.fragment_term_details) {
+class TermDetailsFragment : Fragment(R.layout.fragment_term_details) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
@@ -28,6 +30,17 @@ class TermDetailsFragment: Fragment(R.layout.fragment_term_details) {
         super.onCreateOptionsMenu(menu, inflater)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.star -> Toast.makeText(
+                requireContext(),
+                "Добавлено в избранное",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun setupViews(binding: FragmentTermDetailsBinding) {
         with(binding) {
             subject.title.text = "Раздел"
@@ -38,7 +51,8 @@ class TermDetailsFragment: Fragment(R.layout.fragment_term_details) {
             translation.data.text = "(Перевод)"
 
             imageViewPen.setOnClickListener {
-                materialCardView.strokeColor = ContextCompat.getColor(requireContext(), R.color.grey)
+                materialCardView.strokeColor =
+                    ContextCompat.getColor(requireContext(), R.color.grey)
                 editTextNotes.isFocusableInTouchMode = true
             }
 
--- app/src/main/java/com/example/termdictionary/presentation/ui/terms_list/TermsListFragment.kt
+++ app/src/main/java/com/example/termdictionary/presentation/ui/terms_list/TermsListFragment.kt
@@ -1,12 +1,18 @@
 package com.example.termdictionary.presentation.ui.terms_list
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.termdictionary.R
 import com.example.termdictionary.databinding.FragmentTermsListBinding
-import com.example.termdictionary.domain.model.Term
 import com.example.termdictionary.presentation.model.TermUI
 
 class TermsListFragment : Fragment(R.layout.fragment_terms_list), TermsListAdapter.OnItemClickListener {
@@ -21,11 +27,61 @@ class TermsListFragment : Fragment(R.layout.fragment_terms_list), TermsListAdapt
         TermUI("Аппликата")
     )
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private lateinit var toggle: ActionBarDrawerToggle
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onViewCreated(view, savedInstanceState)
         val binding = FragmentTermsListBinding.bind(view)
+
+        initNavigationDrawer(binding)
+
         binding.termsListRecyclerView.adapter = TermsListAdapter(terms, this)
+    }
+
+    private fun initNavigationDrawer(binding: FragmentTermsListBinding) {
+        toggle = ActionBarDrawerToggle(activity, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true) // for converting hamburger to back arrow
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.chosen_terms -> Toast.makeText(requireContext(), R.string.chosen, Toast.LENGTH_SHORT).show()
+                R.id.test -> Toast.makeText(requireContext(), R.string.test, Toast.LENGTH_SHORT).show()
+                R.id.add_term -> Toast.makeText(requireContext(), R.string.add_term, Toast.LENGTH_SHORT).show()
+                R.id.extract_term -> Toast.makeText(requireContext(), R.string.extract_terms, Toast.LENGTH_SHORT).show()
+                R.id.settings -> Toast.makeText(requireContext(), R.string.settings, Toast.LENGTH_SHORT).show()
+                R.id.import_terms -> Toast.makeText(requireContext(), R.string.import_terms, Toast.LENGTH_SHORT).show()
+                R.id.contact -> Toast.makeText(requireContext(), R.string.contact, Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_terms_list, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?) = true
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Toast.makeText(requireContext(), newText, Toast.LENGTH_SHORT).show()
+                return true
+            }
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) { return true }
+        when(item.itemId) {
+            R.id.action_filter -> {
+                Toast.makeText(requireContext(), "Фильтр", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onItemClick(term: TermUI) {
