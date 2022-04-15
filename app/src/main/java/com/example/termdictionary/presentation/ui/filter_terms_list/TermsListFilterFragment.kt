543a81cc26ebae25dc6cc73cef62ef7d4b480a2e
Firdavs <firdavs_20111@mail.ru>
2022-04-15 21:07:53 +0300

    rounded filter dialog

--- app/src/main/java/com/example/termdictionary/presentation/ui/filter_terms_list/TermsListFilterFragment.kt
+++ app/src/main/java/com/example/termdictionary/presentation/ui/filter_terms_list/TermsListFilterFragment.kt
@@ -1,16 +1,19 @@
 package com.example.termdictionary.presentation.ui.filter_terms_list
 
+import android.app.Dialog
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.DialogFragment
 import com.example.termdictionary.R
 import com.example.termdictionary.databinding.FragmentFilterTermsListBinding
 
-class TermsListFilterFragment: DialogFragment(R.layout.fragment_filter_terms_list) {
+class TermsListFilterFragment : DialogFragment(R.layout.fragment_filter_terms_list) {
 
     private var _binding: FragmentFilterTermsListBinding? = null
     private val binding get() = _binding!!
@@ -23,13 +26,19 @@ class TermsListFilterFragment: DialogFragment(R.layout.fragment_filter_terms_lis
 
         _binding = FragmentFilterTermsListBinding.inflate(inflater, container, false)
 
-        binding.buttonApplyFilter.setOnClickListener{
+        binding.buttonApplyFilter.setOnClickListener {
             val major = binding.autoCompleteTextViewMajor.text
             val subject = binding.autoCompleteTextViewSubject.text
             Toast.makeText(requireContext(), "$major\n$subject", Toast.LENGTH_SHORT).show()
             dismiss()
         }
         return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+        }
     }
 
     override fun onResume() {
@@ -48,7 +57,8 @@ class TermsListFilterFragment: DialogFragment(R.layout.fragment_filter_terms_lis
 
     private fun setSubjectsAdapter() {
         val subjects = resources.getStringArray(R.array.subjects)
-        val subjectsAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_filter_item, subjects)
+        val subjectsAdapter =
+            ArrayAdapter(requireContext(), R.layout.dropdown_filter_item, subjects)
         binding.autoCompleteTextViewSubject.setAdapter(subjectsAdapter)
     }
 }
